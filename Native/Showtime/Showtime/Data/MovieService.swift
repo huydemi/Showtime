c4d90ed2d9a36b83a9a1dab5a76507e836506f11
Dang Quoc Huy <huysamdua@yahoo.com>
2017-09-12 11:16:07 +0700

    Invoking JavaScript Methods

--- Native/Showtime/Showtime/Data/MovieService.swift
+++ Native/Showtime/Showtime/Data/MovieService.swift
@@ -21,15 +21,70 @@
 */
 
 import UIKit
+import JavaScriptCore
 
 let movieUrl = "https://itunes.apple.com/us/rss/topmovies/limit=50/json"
+
+lazy var context: JSContext? = {
+  let context = JSContext()
+  
+  // First, you load the common.js file from the application bundle, which contains the JavaScript code you want to access.
+  guard let
+    commonJSPath = Bundle.main.path(forResource: "common", ofType: "js") else {
+      print("Unable to read resource files.")
+      return nil
+  }
+  
+  // After loading the file, the context object will evaluate its contents by calling context.evaluateScript(), passing in the file contents for the parameter.
+  do {
+    let common = try String(contentsOfFile: commonJSPath, encoding: String.Encoding.utf8)
+    _ = context?.evaluateScript(common)
+  } catch (let error) {
+    print("Error while processing script file: \(error)")
+  }
+  
+  return context
+}()
 
 class MovieService {
   
   func loadMoviesWith(limit: Double, onComplete complete: @escaping ([Movie]) -> ()) {
+    guard let url = URL(string: movieUrl) else {
+      print("Invalid url format: \(movieUrl)")
+      return
+    }
+    
+    URLSession.shared.dataTask(with: url) { data, _, _ in
+      guard let data = data, let jsonString = String(data: data, encoding: String.Encoding.utf8) else {
+        print("Error while parsing the response data.")
+        return
+      }
+      
+      let movies = self.parse(response: jsonString, withLimit: limit)
+      complete(movies)
+      }.resume()
   }
   
   func parse(response: String, withLimit limit: Double) -> [Movie] {
+    // First, you make sure the context object is properly initialized. If there were any errors during the setup (e.g.: common.js was not in the bundle), there’s no point in resuming.
+    guard let context = context else {
+      print("JSContext not found.")
+      return []
+    }
+    
+    // You ask the context object to provide the parseJSON() method. As mentioned previously, the result of the query will be wrapped in a JSValue object. Next, you invoke the method using call(withArguments:), where you specify the arguments in an array format. Finally, you convert the JavaScript value to an array.
+    let parseFunction = context.objectForKeyedSubscript("parseJson")
+    guard let parsed = parseFunction?.call(withArguments: [response]).toArray() else {
+      print("Unable to parse JSON")
+      return []
+    }
+    
+    // filterByLimit() returns the list of movies that fit the given price limit.
+    let filterFunction = context.objectForKeyedSubscript("filterByLimit")
+    let filtered = filterFunction?.call(withArguments: [parsed, limit]).toArray()
+    
+    // So you’ve got the list of movies, but there’s still one missing piece: filtered holds a JSValue array, and you need to map them to the native Movie type.
     return []
   }
+  
 }
